fde7f2cc7ad374341568c126363d3aa5b52ac4c9
josbo12 <josepborras8@gmail.com>
2017-04-19 11:30:37 +0200

    creacio de la classe i def monofasic

--- proves_tkinter.py
+++ proves_tkinter.py
@@ -1,43 +1,65 @@
 #!/usr/bin/python
 from Tkinter import *
 
-def tipus_instalacio():
+class Tkinter(object):
 
-    boto.destroy()
-    lbl.config(text="Selecciona el tipus d'instalacio:")
-    boto1 = Button(app,text="Monofasic",command=monofasic)
-    boto1.place(x=100,y=50)
-    boto2 = Button(app,text="Trifasic",command=trifasic)
-    boto2.place(x=200,y=50)
+        def __init__(self):
+            super(Tkinter, self).__init__()
 
 
-def monofasic():
-    lbl.config(text="Selecciona el nombre de linies que es mesuraran:")
-    boto1 = Button(app,text="Acceptar",command=eleccio_base_dades)
+        def tipus_instalacio(self):
 
-def trifasic():
-    pass
+            self.boto.destroy()
+            self.lbl.config(text="Selecciona el tipus d'instalacio:")
 
-def eleccio_base_dades():
-    pass
-
-app = Tk()
-app.title("Control del consum electric")
-app.geometry("500x300")
-
-#FP -> Finestra Principal
-#vp = Frame(app)
-#vp.grid(column=0,row=0,padx=(50,50),pady=(10,10))
- #vp.columnconfigure(0,weight=1)
- #vp.rowconfigure(0,weight=1)
-
-texto="Instruccions d'us:\n Primer de tot "
-lbl = Label(app,text=texto)
-lbl.place(x=200,y=10)
-
-boto = Button(app,text="Continuar",command=tipus_instalacio)
-boto.place(x=210,y=50)
+            self.boto_mono = Button(self.app,text="Monofasic",command=self.monofasic)
+            self.boto_tri = Button(self.app,text="Trifasic",command=self.trifasic)
+            self.boto_tri.place(x=310,y=50)
+            self.boto_mono.place(x=210,y=50)
 
 
+        def monofasic(self):
+            selec = IntVar()
+            self.radiobutton1 = Radiobutton(self.app,text="Una linia",variable=selec value=1)
+            self.radiobutton2 = Radiobutton(self.app,text="Dos linies",variable=selec value=2)
+            self.radiobutton3 = Radiobutton(self.app,text="Tres linies",variable=selec value=3)
+            self.radiobutton1.place(x=260,y=100)
+            self.radiobutton2.place(x=260,y=150)
+            self.radiobutton3.place(x=260,y=200)
+            self.boto_mono.destroy()
+            self.boto_tri.destroy()
+            self.lbl.config(text="Selecciona el nombre de linies que es mesuraran:")
+            self.boto_dades_mono = Button(self.app,text="Continuar",command=self.eleccio_base_dades)
+            self.boto_dades_mono.place(x=260,y=250)
 
-app.mainloop()
+        def trifasic(self):
+            pass
+
+        def eleccio_base_dades(self):
+
+            if self.rad
+
+
+        def create_window(self):
+
+            self.app = Tk()
+            self.app.title("Control del consum electric")
+            self.app.geometry("500x300")
+
+
+            texto="Instruccions d'us:\n Primer de tot "
+            self.lbl = Label(self.app,text=texto)
+            self.lbl.place(x=200,y=10)
+
+            self.boto = Button(self.app,text="Continuar",command=self.tipus_instalacio)
+            self.boto.place(x=210,y=50)
+
+
+
+            self.app.mainloop()
+
+if __name__ == "__main__":
+
+        print " 'Control of electric consumption' "
+        c = Tkinter()
+        c.create_window()

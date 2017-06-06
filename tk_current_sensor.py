4f0173b005824f3fcec7fe2fc3d53b03afb2cb58
josbo12 <josepborras8@gmail.com>
2017-06-06 11:43:22 +0200

    solventar conflicte

--- tk_current_sensor.py
+++ tk_current_sensor.py
@@ -16,23 +16,23 @@ class PowerServer(object):
 
         def read_sensor(self):
 
-<<<<<<< HEAD
+
                 try:
 
 
-                                                              
-                               ser = serial.Serial('/dev/ttyAMA0', 38400, timeout=1)                               
-			       
+
+                               ser = serial.Serial('/dev/ttyAMA0', 38400, timeout=1)
+
                                response = ser.readline()
                                self.z = response.split(",")
-                               
+
                                self.z = map(float, self.z)
-=======
+
             #    try:
                                self.a = random.randrange(10)
                                self.b = random.randrange(50)
                                self.c = random.randrange(100)
->>>>>>> bbe3a8fe009b5a0b66f25097e454d6f67cf40997
+
 
                                for i in range(len(self.z)):
                                    self.z[i]=int(self.z[i])
@@ -43,24 +43,22 @@ class PowerServer(object):
 
                                    print "power 1 %s watts" % z[2][:-2]
 
-<<<<<<< HEAD
 
 
 
 
-=======
+
+
                 #except KeyboardInterrupt:
                 #    print "Lectura aturada per teclat"
->>>>>>> bbe3a8fe009b5a0b66f25097e454d6f67cf40997
+
 
                 except KeyboardInterrupt:
                     print "Lectura aturada per teclat"
 
-<<<<<<< HEAD
                     ser.close()
 
-=======
->>>>>>> bbe3a8fe009b5a0b66f25097e454d6f67cf40997
+
 if __name__ == "__main__":
 
         c = PowerServer()

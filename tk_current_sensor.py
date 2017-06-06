25b0ba9157252146732f4369ed8c8b384a5f7bbd
Josbo12 <josepborras8@gmail.com>
2017-06-06 09:40:33 +0000

    rpict en funcionament

--- tk_current_sensor.py
+++ tk_current_sensor.py
@@ -5,7 +5,7 @@ import datetime
 import random
 import time
 
-#import serial
+import serial
 
 
 class PowerServer(object):
@@ -16,31 +16,35 @@ class PowerServer(object):
 
         def read_sensor(self):
 
-            #    try:
+                try:
 
 
-                               self.a = random.randrange(10)
-                               self.b = random.randrange(50)
-                               self.c = random.randrange(100)
+                                                              
+                               ser = serial.Serial('/dev/ttyAMA0', 38400, timeout=1)                               
+			       
+                               response = ser.readline()
+                               self.z = response.split(",")
+                               
+                               self.z = map(float, self.z)
 
-                               #response = ser.readline()
-                               #self.z = response.split(",")
-                               #self.z.pop()
-                               #self.z = map(float, self.z)
+                               for i in range(len(self.z)):
+                                   self.z[i]=int(self.z[i])
 
-                               #for i in range(len(self.z)):
-                                #   self.z[i]=int(self.z[i])
+                               if len(self.z)>=2:
+                                   print "power 1 %s watts" % z[0]
+                                   print "power 2 %s watts" % z[1]
 
-                               #if len(self.z)>=2:
-
-
-
-
-
-                #except KeyboardInterrupt:
-                #    print "Lectura aturada per teclat"
+                                   print "power 1 %s watts" % z[2][:-2]
 
 
+
+
+
+
+                except KeyboardInterrupt:
+                    print "Lectura aturada per teclat"
+
+                    ser.close()
 
 if __name__ == "__main__":
 
